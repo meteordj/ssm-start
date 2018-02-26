055349ee91db09e1a37adf751d1e1bd186e1b03a
meteordj <962479623@qq.com>
2018-02-26 11:43:14 +0800

    init

---
+++ src/main/java/com/seecen/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.seecen.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Controller
+public class HelloController{
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: q18idc.com QQ993143799
+  Date: 2018/2/26
+  Time: 10:42
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+Hello Word!
+</body>
+</html>
